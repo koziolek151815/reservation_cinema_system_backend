131964d4f59c50ed878b584664cebd40cfde7cb3
koziolek151815 <koziolek151815x@gmail.com>
2021-07-30 14:33:43 +0200

    init commit

---
+++ src/main/java/com/wat/reservation_cinema_system_backend/ReservationCinemaSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wat.reservation_cinema_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationCinemaSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationCinemaSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wat/reservation_cinema_system_backend/ReservationCinemaSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wat.reservation_cinema_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationCinemaSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
