a6a766b95595e544ccb9dfbf6399c21b35afe42e
koziolek151815 <koziolek151815x@gmail.com>
2021-09-24 21:46:28 +0200

    working swagger

--- src/main/java/com/wat/reservation_cinema_system_backend/config/SwaggerConfig.java
+++ src/main/java/com/wat/reservation_cinema_system_backend/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.wat.reservation_cinema_system_backend"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
