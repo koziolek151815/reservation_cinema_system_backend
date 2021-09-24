4cff8243fc35835a99be0301af538b1ca6bffff4
koziolek151815 <koziolek151815x@gmail.com>
2021-09-24 10:06:58 +0200

    add auditorium with admin role

--- src/main/java/com/wat/reservation_cinema_system_backend/auditorium/AuditoriumController.java
+++ src/main/java/com/wat/reservation_cinema_system_backend/auditorium/AuditoriumController.java
@@ -3,6 +3,7 @@ package com.wat.reservation_cinema_system_backend.auditorium;
 import com.wat.reservation_cinema_system_backend.auditorium.dto.AuditoriumRequestDto;
 import com.wat.reservation_cinema_system_backend.auditorium.dto.AuditoriumResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auditoriums")
 public class AuditoriumController {
     private final AuditoriumService auditoriumService;
+
     @PostMapping
-    public AuditoriumResponseDto addAuditoriumWithSeats(@RequestBody AuditoriumRequestDto auditoriumRequestDto){
+    @PreAuthorize("hasRole('admin')")
+    public AuditoriumResponseDto addAuditoriumWithSeats(@RequestBody AuditoriumRequestDto auditoriumRequestDto) {
         return auditoriumService.addAuditoriumWithSeats(auditoriumRequestDto);
     }
 }
