50374550dcabf454af5d68edf30325de36509843
koziolek151815 <koziolek151815x@gmail.com>
2021-11-01 15:33:18 +0100

    tests

--- src/test/java/com/wat/reservation_cinema_system_backend/controllers/ScreeningControllerTest.java
+++ src/test/java/com/wat/reservation_cinema_system_backend/controllers/ScreeningControllerTest.java
@@ -33,7 +33,7 @@ import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ExampleTest {
+public class ScreeningControllerTest {
 
     @Autowired
     private MockMvc mvc;
@@ -42,34 +42,35 @@ public class ExampleTest {
     private ScreeningService screeningService;
 
     @Test
-    public void givenEmployees_whenGetEmployees_thenReturnJsonArray()
+    public void should_return_screenings_for_non_logged_users()
             throws Exception {
 
         List<MovieScreeningsDayDto> movieScreeningsDayDtoArrayList = new ArrayList<>();
 
-
-        when(screeningService.getMoviesScreeningsForDay(LocalDate.parse("2021-11-17"))).thenReturn(movieScreeningsDayDtoArrayList);
+        when(screeningService.getMoviesScreeningsForDay(LocalDate.parse("2021-11-17")))
+                .thenReturn(movieScreeningsDayDtoArrayList);
 
         mvc.perform(get("/screenings/list").param("date", "2021-11-17")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(print())
                 .andExpect(jsonPath("$", hasSize(0)));
     }
 
     @Test
-    @WithMockUser(roles = {"admin"})
-    public void sdfsdf() throws Exception {
+    @WithMockUser(roles = {"worker"})
+    public void should_return_screenings_for_auditorium_for_worker() throws Exception {
         List<ScreeningResponseDto> screeningResponseDtoList = new ArrayList<>();
-        when(screeningService.getScreeningsForDayAndAuditorium(LocalDate.parse("2021-11-17"),1L)).thenReturn(screeningResponseDtoList);
-        mvc.perform(get("/screenings/screeningDayAndAuditorium").param("date", "2021-11-17").param("auditoriumId","1")
+        when(screeningService.getScreeningsForDayAndAuditorium(LocalDate.parse("2021-11-17"), 1L))
+                .thenReturn(screeningResponseDtoList);
+        mvc.perform(get("/screenings/screeningDayAndAuditorium")
+                        .param("date", "2021-11-17").param("auditoriumId", "1")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());
+                .andExpect(jsonPath("$", hasSize(0)))
+                .andExpect(status().isOk());
     }
     @Test
     @WithMockUser(roles = {"admin"})
-    public void asdf() throws Exception {
+    public void should_add_screening_for_worker() throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         ScreeningRequestDto screeningRequestDto = new ScreeningRequestDto();
         when(screeningService.addScreening(screeningRequestDto)).thenReturn(new ScreeningResponseDto());
