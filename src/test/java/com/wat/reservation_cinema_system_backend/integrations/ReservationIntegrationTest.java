bfa9f0dd339f27ac7a5607bc959443ff19023ed5
koziolek151815 <koziolek151815x@gmail.com>
2021-11-11 00:39:43 +0100

    integration tests

---
+++ src/test/java/com/wat/reservation_cinema_system_backend/integrations/ReservationIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.wat.reservation_cinema_system_backend.integrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ReservationIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(roles = {"admin"})
+    public void should_get_reservations_for_screening_worker() throws Exception {
+        this.mockMvc
+                .perform(get("/reservations/256"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$", hasSize(3)));
+    }
+    @Test
+    @WithMockUser(roles = {"admin"})
+    public void should_change_status_on_paid_worker() throws Exception {
+        this.mockMvc
+                .perform(put("/reservations/342"))
+                .andDo(print()).andExpect(status().isOk());
+    }
+
+}
--- src/test/java/com/wat/reservation_cinema_system_backend/integrations/TicketIntegrationTest.java
+++ src/test/java/com/wat/reservation_cinema_system_backend/integrations/TicketIntegrationTest.java
@@ -4,44 +4,50 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wat.reservation_cinema_system_backend.movie.dto.MovieResponseDto;
 import com.wat.reservation_cinema_system_backend.screening.dto.ScreeningRequestDto;
 import com.wat.reservation_cinema_system_backend.screening.dto.ScreeningResponseDto;
+import com.wat.reservation_cinema_system_backend.ticket.TicketListRequestDto;
+import com.wat.reservation_cinema_system_backend.ticket.TicketRequestDto;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ExampleTest {
+public class TicketIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void testCreateRetrieveWithMockMVC() throws Exception {
-        this.mockMvc
-                .perform(get("/screenings/list").param("date", "2021-11-20"))
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"));
+    @WithMockUser(roles = {"admin"})
+    public void should_book_ticket_worker() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+        List<TicketRequestDto> ticketsList = new ArrayList<>();
+        ticketsList.add(TicketRequestDto.builder().ticketTypeId(1L).auditoriumId(211L).seatNumber(1).seatRow(1).build());
+        TicketListRequestDto ticketListRequestDto = TicketListRequestDto
+                .builder().price(10.50).ticketsList(ticketsList)
+                .build();
 
+        this.mockMvc.perform(post("/tickets/worker/248")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(ticketListRequestDto)))
+                .andExpect(status().is2xxSuccessful());
     }
 
 
 }
+
